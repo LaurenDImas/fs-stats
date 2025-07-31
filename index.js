10b380b231d91a8e1e59073eab0de7bd01a135f9
LaurenDImas <dimas.yogi.777@gmail.com>
2025-07-31 14:47:10 +0700

    add index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// npm login
+import {average as mean, median} from "./stats.js";
+import {readFileSync} from "node:fs";
+
+const fileBuffer = readFileSync("./score.txt", {encoding: 'utf8'})
+const lines = fileBuffer.split('\n');
+const nums = lines.map(line => Number.parseInt(line))
+
+const avg = mean(nums);
+const med = median(nums);
+
+console.log({avg, med})
